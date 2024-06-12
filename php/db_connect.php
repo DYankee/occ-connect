1990ce1e27a10fa5c87b7f9ba9227cf66c1a34ef
zachary geary <zpgeary@gmail.com>
2024-06-12 15:40:08 -0400

    switched to using a composefile for start up

--- php/db_connect.php
+++ php/db_connect.php
@@ -5,10 +5,10 @@ define('DB_HOST', '127.0.0.1'); //Or could be a different server
 define('DB_USER', 'user'); //MySQL credentials
 define('DB_PASSWORD', '1019'); //MySQL credentials 
 define('DB_NAME', 'geary'); //Specific database
-define('DB_PORT', '3306')
+
 
 //Database connection
-$dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
+$dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
        OR die('Connection error');
 
 //Set the encoding
